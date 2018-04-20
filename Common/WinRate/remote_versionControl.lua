ddc6d6d276b7c6ef9653d7e38a75f1e49031cedf
rman200 <padawan200@gmail.com>
2018-04-20 19:58:18 -0400

    Update remote_versionControl.lua

--- Common/WinRate/remote_versionControl.lua
+++ Common/WinRate/remote_versionControl.lua
@@ -1,43 +1,68 @@
-
-local Data = {
-	Core = {1.00,	[[
-		WR Version ChangeLog Here]]
-	},
-	Champions = {
-		["Ashe"] = {
-			1.00, [[
-			Ashe Changelog Here]]
-		},
-		["Corki"] = {
-			1.00, [[
-			Corki Changelog Here]]
-		},
-		["Darius"] = {
-			1.00, [[
-			Darius Changelog Here]]
-		},
-		["Draven"] = {
-			1.00, [[
-			Draven Changelog Here]]
-		},
-		["Sion"] = {
-			1.00, [[
-			Sion Changelog Here]]
-		},
-		["Vladimir"] = {
-			1.00, [[
-			Vladimir Changelog Here]]
-		},
-	},
-	Utilities = {
-		["loader"] = 1.00,
-		["changelog"] 	   = 1.00,
-		["callbacks"] 	   = 1.00,
-		["menuLoad"]  	   = 1.00,
-		["prediction"]     = 1.00,
-		["commonLib"]      = 1.00,
-	}
+Data = {
+    Champions = {
+        Corki = {
+            Version = 1,
+            Changelog = "Corki Changelog Here",
+        },
+        Ashe = {
+            Version = 1,
+            Changelog = "Ashe Changelog Here",
+        },
+        Draven = {
+            Version = 1,
+            Changelog = "Draven Changelog Here",
+        },
+        Darius = {
+            Version = 1,
+            Changelog = "Darius Changelog Here",
+        },
+        Sion = {
+            Version = 1,
+            Changelog = "Sion Changelog Here",
+        },
+        Vladimir = {
+            Version = 1,
+            Changelog = "Vladimir Changelog Here",
+        },
+    },
+    Loader = {
+        Version = 1,
+    },
+    Dependencies = {
+        commonLib = {
+            Version = 1,
+        },
+        prediction = {
+            Version = 1,
+        },
+        changelog = {
+            Version = 1,
+        },
+        callbacks = {
+            Version = 1,
+        },
+        menuLoad = {
+            Version = 1,
+        },
+    },
+    Utilities = {
+        baseult = {
+            Version = 0,
+        },
+        evade = {
+            Version = 0,
+        },
+        tracker = {
+            Version = 0,
+        },
+        orbwalker = {
+            Version = 0,
+        },
+    },
+    Core = {
+        Version = 0,
+        Changelog = "WR Version ChangeLog Here",
+    },
 }
 
 return Data
-
