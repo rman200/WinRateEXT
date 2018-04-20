2fcff4ea376d8c89937848d1744426e1f9fcc3d5
rman200 <padawan200@gmail.com>
2018-04-20 19:34:36 -0400

    Almost ready to release

--- remote_WR.lua
+++ remote_WR.lua
@@ -10,45 +10,50 @@
 
     _G.WR_Loaded = true
 
-    local core_version       = 1.00
-
     local char_name          = myHero.charName 
 
     local open               = io.open
     local concat             = table.concat
+    local rep                = string.rep 
+    local format             = string.format
 
     local WR_PATH            = COMMON_PATH.."WinRate/"
+    local ACTIVE_PATH        = "/Champion Modules/WR_"..char_name
+    local CHAMP_PATH         = WR_PATH..'/Champion Modules'
     local SCRIPT_URL         = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/"
     local WR_URL             = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/"
-    local ACTIVE_PATH        = "/Champion Modules/WR_"..char_name
+    local CHAMP_URL          = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/Champion%20Modules/"
+
     local versionControl     = WR_PATH .. "versionControl.lua"
     local versionControl2    = WR_PATH .. "versionControl2.lua"
-    local wr_module_data     = "wr_module_data.lua"
-    local wr_core            = "wr_core.lua"
-    local wr_check           = "wr_check.lua"  
-    local luaString          = ".lua"  
-    local wr_champ           = "wr_"..char_name..luaString
-    local versionControl_url = WR_URL.."/remote_versionControl.lua"
-    --local wr_champ_url       = "https://raw.githubusercontent.com/HiImWeedle/GoS/master/WinRate/Modules/winrate_"..char_name:lower()..luaString
-
-    
+    local dotlua             = ".lua" 
     
     --WR--
+    local function readAll(file)
+        local f = assert(open(file, "r"))
+        local content = f:read("*all")
+        f:close()
+        return content
+    end
 
     local function AutoUpdate()
-        local function DownloadFile(from, to, filename)
-            DownloadFileAsync(from.."remote_"..filename, to..filename, function() end)            
-            repeat until FileExist(to..filename)
-        end
-        --
-        local function GetVersionControl()
-            --[[First Time Being Run]]
-            if not FileExist(versionControl) then 
-                DownloadFile(WR_URL, WR_PATH, "versionControl.lua") 
-            end             
-            --[[Every Load]]  
-            DownloadFileAsync(WR_URL.."remote_versionControl.lua", versionControl2, function() end)          
-            repeat until FileExist(versionControl2)
+        local function serializeTable(val, name, depth) --recursive function to turn a table into plain text, pls dont mess with this
+            skipnewlines = false
+            depth = depth or 0
+            local res = rep(" ", depth)
+            if name then res = res .. name .. " = " end
+            if type(val) == "table" then
+                res = res .. "{" .. "\n"
+                for k, v in pairs(val) do
+                    res =  res .. serializeTable(v, k, depth + 4) .. "," .. "\n" 
+                end
+                res = res .. rep(" ", depth) .. "}"
+            elseif type(val) == "number" then
+                res = res .. tostring(val)
+            elseif type(val) == "string" then
+                res = res .. format("%q", val)
+            end    
+            return res
         end
         --
         local function TextOnScreen(str)
@@ -57,76 +62,69 @@
                 Draw.Text(str, 64, res.x/2-(#str * 10), res.y/2, Draw.Color(255,255,0,0))
             end)                        
         end
-        local function UpdateLoader()
-            DownloadFile(SCRIPT_URL, SCRIPT_PATH, "WR.lua")
-            TextOnScreen("Please Reload The Script! [F6]x2")
+        --
+        local function DownloadFile(from, to, filename)
+            DownloadFileAsync(from.."remote_"..filename, to..filename, function() end)            
+            repeat until FileExist(to..filename)
+        end
+        --
+        local function GetVersionControl()
+            --[[First Time Being Run]]
+            if not FileExist(versionControl) then 
+                DownloadFileAsync(WR_URL.."remote_versionControl0", versionControl, function() end)          
+                repeat until FileExist(versionControl)
+            end             
+            --[[Every Load]]  
+            DownloadFileAsync(WR_URL.."remote_versionControl.lua", versionControl2, function() end)          
+            repeat until FileExist(versionControl2)
+        end
+        --
+        local function UpdateVersionControl(t)    
+            local str = serializeTable(t, "Data") .. "\n\nreturn Data"    
+            local f = assert(open(versionControl, "w"))
+            f:write(str)
+            f:close()
         end
         --
         local function CheckUpdate()
             local currentData, latestData = dofile(versionControl), dofile(versionControl2)
-            if currentData.Utilities.loader < latestData.Utilities.loader then
-                UpdateLoader()
+            --[[Loader Version Check]]
+            if currentData.Loader.Version < latestData.Loader.Version then
+                DownloadFile(SCRIPT_URL, SCRIPT_PATH, "WR.lua")        
+                currentData.Loader.Version = latestData.Loader.Version
+                TextOnScreen("Please Reload The Script! [F6]x2")
             end
-
-
-            --if not FileExist(COMMON_PATH..wr_check) then 
-            --    CreateUpdateTime()
-            --    return true
-            --end
-            --local file = open(COMMON_PATH..wr_check, "r")
-            --local current_time = os.time()
-            --local check_time = file:read()
-            --file:close()
-            --CreateUpdateTime()
-            --return current_time - check_time >= 300
+            --[[Core Check]]
+            if currentData.Core.Version < latestData.Core.Version then
+                DownloadFile(WR_URL, WR_PATH, "core.lua")
+                currentData.Core.Version = latestData.Core.Version
+            end
+            --[[Active Champ Module Check]]
+            if currentData.Champions[char_name].Version < latestData.Champions[char_name].Version then
+                DownloadFile(CHAMP_URL, CHAMP_PATH, "WR_"..char_name..dotlua)
+                currentData.Champions[char_name].Version = latestData.Champions[char_name].Version
+            end
+            --[[Dependencies Check]]
+            for k,v in pairs(latestData.Dependencies) do
+                if not currentData.Dependencies.k or currentData.Dependencies.k.Version < v.Version then
+                    DownloadFile(WR_URL, WR_PATH, k..dotlua)
+                    currentData.Dependencies.k.Version = v.Version
+                end
+            end
+            --[[Utilities Check]]
+            for k,v in pairs(latestData.Utilities) do
+                if not currentData.Utilities.k or currentData.Utilities.k.Version < v.Version then
+                    DownloadFile(WR_URL, WR_PATH, k..dotlua)
+                    currentData.Utilities.k.Version = v.Version
+                end
+            end
+            UpdateVersionControl(currentData)
         end
-        --
-
-        --
-        --local function GetScriptData()
-        --    local check_version = core_version
-        --    DownloadFile(versionControl_url, versionControl, versionControl)
-        --    --GetVersionControl()
-        --    if check_version ~= core_version then
-        --        DownloadFile(wr_core_url, COMMON_PATH..wr_core, wr_core.." | version: "..core_version) 
-        --        isUpdated = true
-        --    end
-        --end
-        --
-        --local function GetModuleData()
-        --    DownloadFile(wr_module_data_url, COMMON_PATH..wr_module_data, wr_module_data)
-        --    dofile(COMMON_PATH..wr_module_data)
-        --    module_data  = _G.WR_MODULE_DATA 
-        --end
-        --
-        --local function UpdateModules()
-        --    for i, utils in pairs(module_data.utilities) do 
-        --        local wr_utility = "wr_"..utils..luaString
-        --        local wr_utility_url = "https://raw.githubusercontent.com/HiImWeedle/GoS/master/WinRate/Modules/winrate_"..utils..luaString
-        --        DownloadFile(wr_utility_url, COMMON_PATH..wr_utility, wr_utility)
-        --    end 
-        --    if module_data.champions[char_name] == nil then return end
-        --    DownloadFile(wr_champ_url, COMMON_PATH..wr_champ, wr_champ) 
-        --end
-        --
         GetVersionControl()
-        CheckUpdate(currentData, latestData)        
-
-        --    timeCheck = true
-        --    GetScriptData()
-        --    GetModuleData()
-        --    UpdateModules()
-        --end
+        CheckUpdate()
     end
 
-    local function LoadWR() --These 3 functions are only gonna be used here so there's no point of having out of LoadWR()'s chunk
-        local function readAll(file)
-            local f = assert(open(file, "r"))
-            local content = f:read("*all")
-            f:close()
-            return content
-        end
-        --
+    local function LoadWR() --These 2 functions are only gonna be used here so there's no point of having out of LoadWR()'s chunk
         local function clearModule()
             local f = assert(open(WR_PATH.."activeModule.lua", "w"))
             f:write()
@@ -142,10 +140,10 @@
         local dependencies = {"menuLoad", "commonLib", "callbacks", "prediction", ACTIVE_PATH} 
         clearModule()
         for i=1, #dependencies do
-            local dependency = readAll(concat({WR_PATH, dependencies[i], luaString}))
+            local dependency = readAll(concat({WR_PATH, dependencies[i], dotlua}))
             appendModule(dependency)    
         end                          
-        dofile(WR_PATH.."activeModule"..luaString) 
+        dofile(WR_PATH.."activeModule"..dotlua) 
     end
 
     --WR--
