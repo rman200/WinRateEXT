ea21e06c41b83c2399b61337c30702ae35b25447
Unknown <padawan200@gmail.com>
2018-05-06 23:17:35 -0400

    Revert "changelog text"
    This reverts commit d49101e8d9bcb1c6266e8cb8e5093c0713426180.

--- Common/WinRate/remote_versionControl.lua
+++ Common/WinRate/remote_versionControl.lua
@@ -78,16 +78,12 @@ Data = {
     Core = {
         Version = 1.00,
         Changelog = 
-            "Welcome to Project WinRate - The Most Advanced Script Ever on EXT!\n\n"..
-            "We work to deliver the best scripting experience possible through our\n"..
+            "Welcome to Project WinRate - The Most Advanced Script to ever hit EXT!\n\n"..
+            "We aim to deliver the best scripting experience possible through our\n"..
             "solid developers experience and HQ codebase, where creativity reigns\n" ..
             "over most limitations brought by an External Platform. \n\n" ..
-            "Thanks to the seamless implementation of our AutoUpdate system we\n"..
-            "can ensure you're always up to date without any impact on your game\n"..
-            "experience!\n\n\n\n\n\n\n\n"..
-            "                                 If you want to support us, head to the forums where you can upvote\n"..
-            "                                 the thread, leave a nice comment or even make a Donation. "            
-        ,
+            "Thanks to the seamless implementation of our AutoUpdate system we can\n"..
+            "ensure you're always up to date with impacting your gaming experience!",
     },
 }
 
