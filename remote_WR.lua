23f310de577580c239845de310903f4b8138b1e3
Rodrigo N <padawan200@gmail.com>
2018-08-22 01:45:43 -0400

    Loader Update 0.1

--- remote_WR.lua
+++ remote_WR.lua
@@ -8,177 +8,251 @@
         return 
     end
 
+    local Menu
     local open               = io.open
     local concat             = table.concat
     local rep                = string.rep 
     local format             = string.format
     local insert             = table.insert
+    local remove             = table.remove
+
+    local charName           = myHero.charName 
+    local ShouldLoad         = {}
+    local DownloadQueue      = {}
 
     local WR_PATH            = COMMON_PATH.."WinRate/"
-    local dotlua             = ".lua" 
-    local charName           = myHero.charName 
-    local shouldLoad         = {}
+    local CHAMP_PATH         = WR_PATH..'/Champion Modules/'
+    local SCRIPT_URL         = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/"
+    local WR_URL             = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/"
+    local CHAMP_URL          = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/Champion%20Modules/"
+    local versionControl     = WR_PATH .. "versionControl.lua"
+    local versionControl2    = WR_PATH .. "versionControl2.lua"
+ 
+    --WR--
 
-    local function readAll(file)
+    class 'Warn'  
+
+    function Warn:__init(str)
+        if not Menu then
+            self:FirstInstance()  
+        end
+        Menu:MenuElement({id = str, name = "Error:", value = 1,drop = {str}})                            
+    end
+
+    function Warn:FirstInstance()
+        Menu = MenuElement({id = "WR_AutoUpdate", name = "Project WinRate | Error Manager!", type = MENU, leftIcon = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Icons/WinRateLogo.png"})
+        Menu:MenuElement({id = "ErrorHelper1", name = "Oops, Looks Like Something Didnt Work!" , value = 1,drop = {""}})
+        Menu:MenuElement({id = "ErrorHelper2", name = "Try pressing F6x2, if it doesnt solve"  , value = 1,drop = {""}})
+        Menu:MenuElement({id = "ErrorHelper3", name = "Ask on forums for help and I'll try to!", value = 1,drop = {""}})
+        Menu:MenuElement({id = "space"       , name = "", type = SPACE})
+
+        local res = Game.Resolution()
+        Callback.Add("Draw", function()                       
+            Draw.Text("Something Went Wrong!\n Check Shift Menu.", 64, res.x/2-(#str * 10), res.y/2, Draw.Color(255,255,0,0))
+        end)
+    end
+
+    class 'Utils' 
+
+    function Utils:ReadAll(file)
         local f = assert(open(file, "r"))
-        local content = f:read("*all")
-        f:close()
-        return content
-    end
-    
-    --WR--
-    local function AutoUpdate()
-        local CHAMP_PATH = WR_PATH..'/Champion Modules/'
-        local SCRIPT_URL = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/"
-        local WR_URL     = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/"
-        local CHAMP_URL  = "https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/Champion%20Modules/"
-        local versionControl     = WR_PATH .. "versionControl.lua"
-        local versionControl2    = WR_PATH .. "versionControl2.lua"
-        --
-        local function serializeTable(val, name, depth) --recursive function to turn a table into plain text, pls dont mess with this
-            skipnewlines = false
-            depth = depth or 0
-            local res = rep(" ", depth)
-            if name then res = res .. name .. " = " end
-            if type(val) == "table" then
-                res = res .. "{" .. "\n"
-                for k, v in pairs(val) do
-                    res =  res .. serializeTable(v, k, depth + 4) .. "," .. "\n" 
-                end
-                res = res .. rep(" ", depth) .. "}"
-            elseif type(val) == "number" then
-                res = res .. tostring(val)
-            elseif type(val) == "string" then
-                res = res .. format("%q", val)
-            end    
-            return res
-        end
-        --
-        local function TextOnScreen(str)
-            local res = Game.Resolution() 
-            Callback.Add("Draw", function()                       
-                Draw.Text(str, 64, res.x/2-(#str * 10), res.y/2, Draw.Color(255,255,0,0))
-            end)                        
-        end
-        --
-        local function CheckFolders()
-            local f = open(CHAMP_PATH.."folderTest", "w")
-            if f then
-                f:close()
-                return true 
-            end
-            TextOnScreen("Check Installation Instructions on Forum!")
-        end
-        --
-        local function DownloadFile(from, to, filename)
-            DownloadFileAsync(from.."remote_"..filename, to..filename, function() end)            
-            repeat until FileExist(to..filename)
-        end
-        --
-        local function GetVersionControl()
-            --[[First Time Being Run]]
-            if not FileExist(versionControl) then 
-                DownloadFileAsync(WR_URL.."remote_versionControl0", versionControl, function() end)          
-                repeat until FileExist(versionControl)
-            end             
-            --[[Every Load]]  
-            DownloadFileAsync(WR_URL.."remote_versionControl.lua", versionControl2, function() end)          
-            repeat until FileExist(versionControl2)
-        end
-        --
-        local function UpdateVersionControl(t)    
-            local str = serializeTable(t, "Data") .. "\n\nreturn Data"    
-            local f = assert(open(versionControl, "w"))
-            f:write(str)
+        if f then
+            local content = f:read("*all")
             f:close()
+            return content
+        end
+    end
+
+    function Utils:SerializeTable(val, name, depth) --recursive function to turn a table into plain text, pls dont mess with this
+        skipnewlines = false
+        depth = depth or 0
+        local res = rep(" ", depth)
+        if name then res = res .. name .. " = " end
+        if type(val) == "table" then
+            res = res .. "{" .. "\n"
+            for k, v in pairs(val) do
+                res =  res .. self:SerializeTable(v, k, depth + 4) .. "," .. "\n" 
+            end
+            res = res .. rep(" ", depth) .. "}"
+        elseif type(val) == "number" then
+            res = res .. tostring(val)
+        elseif type(val) == "string" then
+            res = res .. format("%q", val)
+        end    
+        return res
+    end
+
+    function Utils:CheckRights()
+        --
+        --[[Checks Write Permission]]
+        local  WRITE_PERMISSION_TEST = assert(open(COMMON_PATH.."WR_CHECK_RIGHTS", "w"))
+        if not WRITE_PERMISSION_TEST then
+            Warn("GOS EXT DOESNT HAVE WRITE RIGHTS! LAUNCH AS ADMIN!")
+            return false 
+        end
+        WRITE_PERMISSION_TEST:close()
+        --
+        --[[Checks Read Permission]]
+        local  READ_PERMISSION_TEST = assert(FileExist(COMMON_PATH.."WR_CHECK_RIGHTS"))
+        if not READ_PERMISSION_TEST then
+            Warn("GOS EXT DOESNT HAVE READ RIGHTS! LAUNCH AS ADMIN!")
+            return false 
         end
         --
-        local function CheckUpdate()
-            local currentData, latestData = dofile(versionControl), dofile(versionControl2)
-            --[[Loader Version Check]]
-            if currentData.Loader.Version < latestData.Loader.Version then
-                DownloadFile(SCRIPT_URL, SCRIPT_PATH, "WR.lua")        
-                currentData.Loader.Version = latestData.Loader.Version
-                TextOnScreen("Please Reload The Script! [F6]x2")                
-            end
-            --[[Core Check]]
-            if currentData.Core.Version < latestData.Core.Version then
-                --DownloadFile(WR_URL, WR_PATH, "core.lua")
-                currentData.Core.Version = latestData.Core.Version
-                currentData.Core.Changelog = latestData.Core.Changelog
-            end
-            --[[Active Champ Module Check]]            
-            if not currentData.Champions[charName] or currentData.Champions[charName].Version < latestData.Champions[charName].Version then
-                DownloadFile(CHAMP_URL, CHAMP_PATH, "WR_"..charName..dotlua)
-                currentData.Champions[charName] = {}
-                currentData.Champions[charName].Version = latestData.Champions[charName].Version
-                currentData.Champions[charName].Changelog = latestData.Champions[charName].Changelog
-            end
-            --[[Dependencies Check]]
-            for k,v in pairs(latestData.Dependencies) do                
-                if not currentData.Dependencies[k] or currentData.Dependencies[k].Version < v.Version then
-                    DownloadFile(WR_URL, WR_PATH, k..dotlua)
-                    currentData.Dependencies[k] = {}
-                    currentData.Dependencies[k].Version = v.Version
-                end
-                local name = tostring(k)
-                if v.Version >=1 and name ~= "commonLib" and name ~= "changelog" and name ~= "menuLoad" then
-                    shouldLoad[#shouldLoad+1] = name
-                end
-            end
-            --[[Utilities Check]]
-            for k,v in pairs(latestData.Utilities) do
-                if not currentData.Utilities[k] or currentData.Utilities[k].Version < v.Version then
-                    DownloadFile(WR_URL, WR_PATH, k..dotlua)
-                    currentData.Utilities[k] = {}
-                    currentData.Utilities[k].Version = v.Version
-                end
-                if v.Version >=1 then
-                    shouldLoad[#shouldLoad+1] = tostring(k)
-                end
-            end
-            table.sort(shouldLoad)
-            insert(shouldLoad, 1, "commonLib")
-            insert(shouldLoad, 2, "menuLoad")
-            insert(shouldLoad, "/Champion Modules/WR_"..charName)
-            UpdateVersionControl(currentData)
+        --[[Checks Download Permission]]
+        DownloadFileAsync("https://raw.githubusercontent.com/rman200/WinRateEXT/master/Common/WinRate/DL_PERMISSION_TEST.lua", COMMON_PATH.."WR_CHECK_DL", function() end)
+        local  DL_PERMISSION_TEST = assert(FileExist(COMMON_PATH.."WR_CHECK_DL"))
+        if not DL_PERMISSION_TEST then
+            Warn("GOS EXT DOESNT HAVE DOWNLOAD RIGHTS! CHECK FIREWALL!")
+            return false 
         end
-        local function CheckSupported()
-            local Data = dofile(versionControl2)
-            return Data.Champions[charName]
+        --
+        return true
+    end
+
+    function Utils:CheckFolders()
+        local f = open(CHAMP_PATH.."folderTest", "w")
+        if f then
+            f:close()
+            return true 
         end
-        if CheckFolders() then
-            GetVersionControl()
-            if CheckSupported() then  
-                CheckUpdate()
-                return true
+        Warn("Couldnt Find Installation Folders!")
+    end
+
+    function Utils:DownloadFile(from, to, filename)
+        DownloadFileAsync(from.."remote_"..filename, to..filename, function() end) 
+        insert(DownloadQueue, to..filename)       
+    end
+
+    function Utils:CheckSupported()
+        local Data = dofile(versionControl2)
+        return Data.Champions[charName]
+    end
+
+    class 'Updater'  
+
+    function Updater:__init()
+        self.Stage = 0;
+        Callback.Add("Tick", function() self:OnTick() end)
+        --
+        if Utils:CheckRights() and Utils:CheckFolders() then
+            self:GetVersionControl()
+            DelayAction(function() self.Stage = 1 end, 0.2)
+        end
+    end
+
+    function Updater:GetVersionControl()
+        --[[First Time Being Run]]
+        if not FileExist(versionControl) then 
+            DownloadFileAsync(WR_URL.."remote_versionControl0", versionControl, function() end)       
+        end             
+        --[[Every Load]]  
+        DownloadFileAsync(WR_URL.."remote_versionControl.lua", versionControl2, function() end)        
+    end
+    --
+    function Updater:UpdateVersionControl(t)    
+        local str = Utils:SerializeTable(t, "Data") .. "\n\nreturn Data"    
+        local f = assert(open(versionControl, "w"))
+        f:write(str)
+        f:close()
+    end
+    --
+    function Updater:CheckUpdate()
+        DelayAction(function() self.Stage = 2 end, 0.2)
+        local currentData, latestData = dofile(versionControl), dofile(versionControl2)
+        --[[Loader Version Check]]
+        if currentData.Loader.Version < latestData.Loader.Version then
+            Utils:DownloadFile(SCRIPT_URL, SCRIPT_PATH, "WR.lua")        
+            currentData.Loader.Version = latestData.Loader.Version
+            Warn("Please Reload The Script! [F6]x2")                
+        end
+        --[[Active Champ Module Check]]            
+        if not currentData.Champions[charName] or currentData.Champions[charName].Version < latestData.Champions[charName].Version then
+            Utils:DownloadFile(CHAMP_URL, CHAMP_PATH, "WR_"..charName..".lua")
+            currentData.Champions[charName] = {}
+            currentData.Champions[charName].Version   = latestData.Champions[charName].Version
+            currentData.Champions[charName].Changelog = latestData.Champions[charName].Changelog
+        end
+        --[[Dependencies Check]]
+        for k,v in pairs(latestData.Dependencies) do                
+            if not currentData.Dependencies[k] or currentData.Dependencies[k].Version < v.Version then
+                Utils:DownloadFile(WR_URL, WR_PATH, k..".lua")
+                currentData.Dependencies[k] = {}
+                currentData.Dependencies[k].Version = v.Version
+            end
+            local name = tostring(k)
+            if v.Version >=1 and name ~= "commonLib" and name ~= "changelog" and name ~= "menuLoad" then
+                ShouldLoad[#ShouldLoad+1] = name
+            end
+        end
+        --[[Utilities Check]]
+        for k,v in pairs(latestData.Utilities) do
+            if not currentData.Utilities[k] or currentData.Utilities[k].Version < v.Version then
+                Utils:DownloadFile(WR_URL, WR_PATH, k..".lua")
+                currentData.Utilities[k] = {}
+                currentData.Utilities[k].Version = v.Version
+            end
+            if v.Version >=1 then
+                ShouldLoad[#ShouldLoad+1] = tostring(k)
+            end
+        end
+        table.sort(ShouldLoad)
+        insert(ShouldLoad, 1, "commonLib")
+        insert(ShouldLoad, 2, "menuLoad")
+        insert(ShouldLoad, "/Champion Modules/WR_"..charName)
+        self:UpdateVersionControl(currentData)
+    end
+
+    function Updater:OnTick()
+        if _G.WR_Loaded then return end
+        --
+        if self.Stage == 1 and FileExist(versionControl) and FileExist(versionControl2) then
+            if Utils:CheckSupported() then  
+                self:CheckUpdate()                
+            else
+                self.Stage = -1
+            end                        
+        elseif self.Stage == 2 then        
+            local DownloadsLeft = 0;
+            for k, v in pairs(DownloadQueue) do
+                DownloadsLeft = DownloadsLeft + 1
+                if FileExist(v) then
+                    remove(DownloadQueue, k)
+                    DownloadsLeft = DownloadsLeft - 1
+                end
+            end
+            if DownloadsLeft == 0 then
+                _G.WR_Loaded = true             
+                Loader()
             end
         end
     end
 
-    local function LoadWR()         
-        local function writeModule(content)            
-            local f = assert(open(WR_PATH.."activeModule.lua", content and "a" or "w"))
-            if content then
-                f:write(content)
-            end
-            f:close()        
-        end
-        --        
-        writeModule()
-        for i=1, #shouldLoad do
-            local dependency = readAll(concat({WR_PATH, shouldLoad[i], dotlua}))
+    class 'Loader'  
+
+    function Loader:__init()               
+        self:WriteModule()
+        for i=1, #ShouldLoad do
+            local dependency = Utils:ReadAll(concat({WR_PATH, ShouldLoad[i], ".lua"}))
             writeModule(dependency)    
-        end                          
-        dofile(WR_PATH.."activeModule"..dotlua) 
+        end
+
+        dofile(WR_PATH.."changelog"..".lua")                          
+        dofile(WR_PATH.."activeModule"..".lua") 
+    end
+
+    function Loader:WriteModule(content)            
+        local f = assert(open(WR_PATH.."activeModule.lua", content and "a" or "w"))
+        if content then
+            f:write(content)
+        end
+        f:close()
     end
 
     --WR--
 
-    function OnLoad()   
-        if AutoUpdate() then
-            _G.WR_Loaded = true
-            dofile(WR_PATH.."changelog"..dotlua) 
-            LoadWR()
-        end
+    function OnLoad()
+        Updater()
     end    
+
+
